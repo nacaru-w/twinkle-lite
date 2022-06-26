a090d9fbb4d91ad9a5fc003538b8f3f6782af4b1
nasch92 <ignaciocr92@gmail.com>
2022-06-26 17:59:25 +0200

    Autoformatting

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 //<syntaxhighlight lang="javascript">
-mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript' );
-mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css' );
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript');
+mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
 
 
 console.log("Loading Deletion Request Maker...");
 
 let listOptions = [
-	{ code:'B', name:'Biografías'},
-	{ code:'CAT', name:'Categorías'},
-	{ code:'D', name:'Deportes y juegos'},
-	{ code:'F', name:'Ficción y artes'},
-	{ code:'I', name:'Inclasificables'},
-	{ code:'L', name:'Lugares y transportes'},
-	{ code:'M', name:'Música y medios de comunicación'},
-	{ code:'N', name:'Consultas sin clasificar todavía'},
-	{ code:'O', name:'Organizaciones, empresas y productos'},
-	{ code:'P', name:'Plantillas y userboxes'},
-	{ code:'S', name:'Sociedad'},
-	{ code:'T', name:'Ciencia y tecnología'},
-	{ code:'W', name:'Web e internet'}
+	{ code: 'B', name: 'Biografías' },
+	{ code: 'CAT', name: 'Categorías' },
+	{ code: 'D', name: 'Deportes y juegos' },
+	{ code: 'F', name: 'Ficción y artes' },
+	{ code: 'I', name: 'Inclasificables' },
+	{ code: 'L', name: 'Lugares y transportes' },
+	{ code: 'M', name: 'Música y medios de comunicación' },
+	{ code: 'N', name: 'Consultas sin clasificar todavía' },
+	{ code: 'O', name: 'Organizaciones, empresas y productos' },
+	{ code: 'P', name: 'Plantillas y userboxes' },
+	{ code: 'S', name: 'Sociedad' },
+	{ code: 'T', name: 'Ciencia y tecnología' },
+	{ code: 'W', name: 'Web e internet' }
 ];
 
 let nominatedPageName = mw.config.get('wgPageName')
@@ -29,26 +29,26 @@ function canCreateDeletionRequestPage() {
 }
 
 function isPageMissing(title) {
-    let params = {
-        action: 'query',
-        titles: title,
-        prop: 'pageprops',
-        format: 'json'
-    };
-    let apiPromise = new mw.Api().get(params);
-    return apiPromise.then(function (data) {
-        let result = data.query.pages
+	let params = {
+		action: 'query',
+		titles: title,
+		prop: 'pageprops',
+		format: 'json'
+	};
+	let apiPromise = new mw.Api().get(params);
+	return apiPromise.then(function (data) {
+		let result = data.query.pages
 		return result.hasOwnProperty("-1")
-    });
+	});
 }
 
 function userFromGetReply(data) {
 	let pages = data.query.pages,
 		p;
-	for ( p in pages ) {
-		return pages[ p ].revisions[ 0 ].user;
+	for (p in pages) {
+		return pages[p].revisions[0].user;
 	}
-} 
+}
 
 // Returns a promise with the name of the user who created the page
 function getCreator() {
@@ -69,8 +69,8 @@ function getCreator() {
 
 function getCategoryOptions() {
 	let categoryOptions = [];
-	for (let category of listOptions){
-		let option = {type:'option', value: category.code, label:category.name};
+	for (let category of listOptions) {
+		let option = { type: 'option', value: category.code, label: category.name };
 		categoryOptions.push(option);
 	}
 	return categoryOptions;
@@ -93,11 +93,11 @@ function createWindow() {
 		label: 'Aceptar'
 	});
 	form.append({
-				type: 'select',
-				name: 'category',
-				label: 'Selecciona la categoría de la página:',
-				list: getCategoryOptions()
-			});
+		type: 'select',
+		name: 'category',
+		label: 'Selecciona la categoría de la página:',
+		list: getCategoryOptions()
+	});
 	let result = form.render();
 	Window.setContent(result);
 	Window.display();
@@ -109,36 +109,36 @@ function submitMessage(e) {
 	if (input.reason === ``) {
 		alert("No se ha establecido un motivo.");
 	} else {
-        if (window.confirm(`Esto creará una consulta de borrado para el artículo ${nominatedPageName}, ¿estás seguro?`)) {
-			console.log ( 'Making sure another DR is not ongoing...');
+		if (window.confirm(`Esto creará una consulta de borrado para el artículo ${nominatedPageName}, ¿estás seguro?`)) {
+			console.log('Making sure another DR is not ongoing...');
 			canCreateDeletionRequestPage()
-			.then( function (canMakeNewDeletionRequest) {
-				if (!canMakeNewDeletionRequest) {
-					throw new Error( 'La página no puede crearse. Ya existe una candidatura en curso o esta se cerró en el pasado.' ) 
-				} else {
-            		return new mw.Api().edit(
-                	nominatedPageName,
-                	buildEditOnNominatedPage
-            		);
-				}
-			})
-			.then( function () {
-                console.log( 'Creating deletion request page...' );
-				return createDeletionRequestPage(input.category, input.reason);
-			})
-			.then( function () {
-				console.log( 'Dropping a message on the creator\'s talk page...');
-				return getCreator().then(postsMessage);
-			})
-			.then( function () {
-				console.log('Refreshing...');
-				location.reload();
-			})
-			.catch( function (error) {
-				console.log('Aborted: nomination page already exists');
-				alert(error.message);
-			})
-        }
+				.then(function (canMakeNewDeletionRequest) {
+					if (!canMakeNewDeletionRequest) {
+						throw new Error('La página no puede crearse. Ya existe una candidatura en curso o esta se cerró en el pasado.')
+					} else {
+						return new mw.Api().edit(
+							nominatedPageName,
+							buildEditOnNominatedPage
+						);
+					}
+				})
+				.then(function () {
+					console.log('Creating deletion request page...');
+					return createDeletionRequestPage(input.category, input.reason);
+				})
+				.then(function () {
+					console.log('Dropping a message on the creator\'s talk page...');
+					return getCreator().then(postsMessage);
+				})
+				.then(function () {
+					console.log('Refreshing...');
+					location.reload();
+				})
+				.catch(function (error) {
+					console.log('Aborted: nomination page already exists');
+					alert(error.message);
+				})
+		}
 
 	}
 }
@@ -150,50 +150,50 @@ function buildDeletionTemplate(category, reason) {
 
 //function that fetches the two functions above and actually adds the text to the article to be submitted to DR.
 function buildEditOnNominatedPage(revision) {
-    return {
-        text: '{{sust:cdb}}\n' + revision.content,
-        summary: `Nominada para su borrado, véase [[Wikipedia:Consultas de borrado/${nominatedPageName}]] mediante [[WP:Deletion Request Maker|Deletion Request Maker]].`,
-        minor: false
-    };
+	return {
+		text: '{{sust:cdb}}\n' + revision.content,
+		summary: `Nominada para su borrado, véase [[Wikipedia:Consultas de borrado/${nominatedPageName}]] mediante [[WP:Deletion Request Maker|Deletion Request Maker]].`,
+		minor: false
+	};
 }
 
 //function that creates the page hosting the deletion request
 function createDeletionRequestPage(category, reason) {
-	return new mw.Api().create(`Wikipedia:Consultas de borrado/${nominatedPageName}`, 
-	{ summary: `Creando página de discusión para el borrado de [[${nominatedPageName}]] mediante [[WP:Deletion Request Maker|Deletion Request Maker]]`},
-	buildDeletionTemplate(category, reason)
+	return new mw.Api().create(`Wikipedia:Consultas de borrado/${nominatedPageName}`,
+		{ summary: `Creando página de discusión para el borrado de [[${nominatedPageName}]] mediante [[WP:Deletion Request Maker|Deletion Request Maker]]` },
+		buildDeletionTemplate(category, reason)
 	);
 }
 
 // Leaves a message on the creator's talk page
 function postsMessage(creator) {
- return isPageMissing(`Usuario_discusión:${creator}`)
- 		.then( function (mustCreateNewTalkPage) {
-	if (mustCreateNewTalkPage) { 
-		return new mw.Api().create(
-			`Usuario_discusión:${creator}`,
-			{ summary: 'Aviso al usuario de la apertura de una CDB mediante [[WP:Deletion Request Maker|Deletion Request Maker]]'},
-			`{{sust:Aviso cdb|${nominatedPageName}}} ~~~~`
-			);
- 		} else {
-			return new mw.Api().edit(
-				`Usuario_discusión:${creator}`,
+	return isPageMissing(`Usuario_discusión:${creator}`)
+		.then(function (mustCreateNewTalkPage) {
+			if (mustCreateNewTalkPage) {
+				return new mw.Api().create(
+					`Usuario_discusión:${creator}`,
+					{ summary: 'Aviso al usuario de la apertura de una CDB mediante [[WP:Deletion Request Maker|Deletion Request Maker]]' },
+					`{{sust:Aviso cdb|${nominatedPageName}}} ~~~~`
+				);
+			} else {
+				return new mw.Api().edit(
+					`Usuario_discusión:${creator}`,
 					function (revision) {
 						return {
 							text: revision.content + `\n{{sust:Aviso cdb|${nominatedPageName}}} ~~~~`,
 							summary: 'Aviso al usuario de la apertura de una CDB mediante [[WP:Deletion Request Maker|Deletion Request Maker]]',
 							minor: false
-							}
 						}
-					)
+					}
+				)
 			}
- 	})
+		})
 }
 
 if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
 	console.log("Special or non-existent page: DRM will therefore not be loaded.");
 } else {
-	let portletLink = mw.util.addPortletLink( 'p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página' );
+	let portletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
 	portletLink.onclick = createWindow;
 }
 
