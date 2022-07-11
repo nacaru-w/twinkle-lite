89300895a3d10a7b4b6c6bc7f20b4ccdc722706d
nasch92 <ignaciocr92@gmail.com>
2022-07-11 23:31:23 +0200

    Reverting last change

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//<syntaxhighlight lang="javascript">
 mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript');
 mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
 
@@ -213,3 +214,6 @@ if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
 	let portletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
 	portletLink.onclick = createFormWindow;
 }
+
+
+//</syntaxhighlight>
