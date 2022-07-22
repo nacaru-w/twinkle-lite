85f609faf449a1b3aef55a06530d6ebc520763ce
nasch92 <ignaciocr92@gmail.com>
2022-07-22 23:59:21 +0200

    Adding form window prototype

--- src/modules/reports.js
+++ src/modules/reports.js
@@ -1,8 +1,16 @@
 import * as utils from './utils';
 
+function createFormWindow() {
+	let Window = new Morebits.simpleWindow(620, 530);
+	Window.setTitle('Solicitar borrado rápido');
+	Window.addFooterLink('Criterios para el borrado rápido', 'Wikipedia:Criterios para el borrado rápido');
+	let form = new Morebits.quickForm(submitMessage);
 
 
-
+    
+	Window.setContent(result);
+	Window.display();
+}
 
 
 
