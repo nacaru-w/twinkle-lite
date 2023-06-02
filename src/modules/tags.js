01b3728257ab5003d5ed5b41ba957c6ab8e818c8
nacaru-w <ignaciocr92@gmail.com>
2023-06-03 00:42:22 +0200

    Adding entries to dictionary

--- src/modules/tags.js
+++ src/modules/tags.js
@@ -23,6 +23,177 @@ const templateDict = [
 	}
 ]
 
+const articleDict = [
+	{
+		code: "autotrad",
+		description: "Uso de automatismo en traducciones de nula calidad"
+	},
+	{
+		code: "categorizar",
+		description: "Artículos sin categorías"
+	},
+	{
+		code: "contextualizar",
+		description: "El tema o ámbito no está claramente redactado"
+	},
+	{
+		code: "complejo",
+		description: "Textos difíciles de entender"
+	},
+	{
+		code: "curiosidades",
+		description: "Textos con sección de curiosidades"
+	},
+	{
+		code: "desactualizado",
+		description: "página con información obsoleta"
+	},
+	{
+		code: "en desarrollo",
+		description: "Páginas en construcción o siendo editadas activamente"
+	},
+	{
+		code: "evento actual",
+		description: ""
+	},
+	{
+		code: "ficticio",
+		description: "Sin enfoque desde un punto de vista real"
+	},
+	{
+		code: "formato de referencias",
+		description: "Referencias incorrectas o mal construidas"
+	},
+	{
+		code: "fuente primaria",
+		description: "Información no verificable"
+	},
+	{
+		code: "fusionar",
+		descripción: "Sugerir una fusión",
+		subgroup: [
+			{
+				type: 'input',
+				name: 'exampleInput',
+				parameter: '1',
+				label: 'Artículo objetivo'
+			}
+		]
+	},
+	{
+		code: "globalizar",
+		description: "Existe sesgo territorial",
+		subgroup: [
+			{
+				type: 'input',
+				name: 'exampleInput',
+				parameter: '1',
+				label: 'Cultura o territorio'
+			}
+		]
+	},
+	{
+		code: "infraesbozo",
+		description: "Contenido insuficiente como para constituir un esbozo de artículo o anexo válido",
+	},
+	{
+		code: "mal traducido",
+		description: "Escasa calidad de una traducción de otro idioma"
+	},
+	{
+		code: "mejorar redacción",
+		description: "Redacciones que no siguen el manual de estilo"
+	},
+	{
+		code: "no neutralidad",
+		description: "Artículos sesgados o claramente decantados en una dirección",
+		subgroup: [
+			{
+				type: 'input',
+				name: 'exampleInput',
+				parameter: '1',
+				label: 'Razón del sesgo',
+				tooltip: 'Desarrolla las razones del sesgo. Es importante, también, desarrollarlas exhaustivamente en la PD',
+				required: true
+			}
+		]
+	},
+	{
+		code: "plagio",
+		description: "artículos copiados, que violan derechos de autor",
+		subgroup: [
+			{
+				type: 'input',
+				name: 'exampleInput',
+				parameter: '1',
+				label: 'URL origen del plagio',
+				required: true
+			}
+		]
+	},
+	{
+		code: "polémico",
+		description: "Temas susceptibles a guerras de edición o vandalismo"
+	},
+	{
+		code: "promocional",
+		description: "Texto con marcado carácter publicitario, no neutral"
+	},
+	{
+		code: "publicidad",
+		description: "Contenido comercial que defiende proselitismos o propaganda"
+	},
+	{
+		code: "PVfan",
+		description: "Escritos poco neutrales, con punto de vista fanático"
+	},
+	{
+		code: "referencias",
+		description: "Artículos sin una sola referencia"
+	},
+	{
+		code: "referencias adicionales",
+		description: "Artículos con falta de referencias"
+	},
+	{
+		code: "renombrar",
+		description: "Para proponer un renombrado de una página",
+		subgroup: [
+			{
+				type: 'input',
+				name: 'exampleInput',
+				parameter: '1',
+				label: 'URL origen del plagio',
+				required: true
+			}
+		]
+	},
+	{
+		code: "revisar traducción",
+		description: "texto traducido legible pero necesita un repaso lingüístico"
+	},
+	{
+		code: "wikificar",
+		description: "Textos con mal formato o que no cumplen el manual de estilo"
+	},
+]
+
+const PDDict = [
+	{
+		code: "discusión protegida",
+		description: "PDs protegidas ante edición"
+	},
+	{
+		code: "no es un foro",
+		description: "Discusiones con muchas conversaciones irrelevantes"
+	},
+	{
+		code: "página usuario",
+		description: "Para páginas de usuario"
+	}
+
+]
+
 function linkBuilder(link) {
 	let fullLink = `https://es.wikipedia.org/wiki/Plantilla:${link}`
 	return `<a href="${fullLink}" target="_blank">(+)</a>`
