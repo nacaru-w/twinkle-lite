b928502d88340884f6b442f0bdb8bb4f657a6f9a
nacaru-w <ignaciocr92@gmail.com>
2023-06-17 01:44:53 +0200

    Adding new approach to TL loading through promises

--- src/index.js
+++ src/index.js
@@ -4,31 +4,83 @@ import * as SpeedyDeletion from "./modules/speedydeletion";
 import * as Reports from "./modules/reports";
 import * as Tags from "./modules/tags";
 
-mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript');
-mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
+// mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript');
+// mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
 
-console.log("Loading Twinkle Lite...");
+// console.log("Loading Twinkle Lite...");
 
-if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
-	console.log("Special or non-existent page: PP will therefore not be loaded.");
-	console.log("Special or non-existent page: DRM will therefore not be loaded.");
-	console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.")
-} else {
-	let DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
-	DRMportletLink.onclick = DeletionRequestMaker.createFormWindow;
-	let PPportletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
-	PPportletLink.onclick = PageProtection.createFormWindow;
-	let SDportletLink = mw.util.addPortletLink('p-cactions', '#', 'Borrado rápido', 'example-button', 'Solicita el borrado rápido de la página');
-	SDportletLink.onclick = SpeedyDeletion.createFormWindow;
-	if (mw.config.get('wgNamespaceNumber') === 0) {
-		let TportleltLink = mw.util.addPortletLink('p-cactions', '#', 'Añadir plantilla', 'example-button', 'Añade una plantilla a la página');
-		TportleltLink.onclick = Tags.createFormWindow;
+// if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
+// 	console.log("Special or non-existent page: PP will therefore not be loaded.");
+// 	console.log("Special or non-existent page: DRM will therefore not be loaded.");
+// 	console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.")
+// } else {
+// 	let DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
+// 	DRMportletLink.onclick = DeletionRequestMaker.createFormWindow;
+// 	let PPportletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
+// 	PPportletLink.onclick = PageProtection.createFormWindow;
+// 	let SDportletLink = mw.util.addPortletLink('p-cactions', '#', 'Borrado rápido', 'example-button', 'Solicita el borrado rápido de la página');
+// 	SDportletLink.onclick = SpeedyDeletion.createFormWindow;
+// 	if (mw.config.get('wgNamespaceNumber') === 0) {
+// 		let TportleltLink = mw.util.addPortletLink('p-cactions', '#', 'Añadir plantilla', 'example-button', 'Añade una plantilla a la página');
+// 		TportleltLink.onclick = Tags.createFormWindow;
+// 	}
+// }
+
+// if (mw.config.get('wgNamespaceNumber') === 2 || mw.config.get('wgNamespaceNumber') === 3 || (mw.config.get('wgPageName').indexOf("Especial:Contribuciones") > -1)) {
+// 	let RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'example-button', 'Informa de un problema en relación con el usuario');
+// 	RportletLink.onclick = Reports.createFormWindow;
+// } else {
+// 	console.log("Non-user page: Reports will therefore not be loaded.")
+// }
+
+const loadMoreBits = () => {
+	return new Promise((resolve, reject) => {
+		mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript', 'text/javascript', resolve, reject);
+	});
+}
+
+const loadMoreBitsCSS = () => {
+	return new Promise((resolve, reject) => {
+		mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css', resolve, reject)
+	});
+}
+
+const initializeTwinkleLite = () => {
+	console.log("Loading Twinkle Lite...");
+
+	if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
+		console.log("Special or non-existent page: PP will therefore not be loaded.");
+		console.log("Special or non-existent page: DRM will therefore not be loaded.");
+		console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.")
+	} else {
+		let DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
+		DRMportletLink.onclick = DeletionRequestMaker.createFormWindow;
+		let PPportletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
+		PPportletLink.onclick = PageProtection.createFormWindow;
+		let SDportletLink = mw.util.addPortletLink('p-cactions', '#', 'Borrado rápido', 'example-button', 'Solicita el borrado rápido de la página');
+		SDportletLink.onclick = SpeedyDeletion.createFormWindow;
+		if (mw.config.get('wgNamespaceNumber') === 0) {
+			let TportleltLink = mw.util.addPortletLink('p-cactions', '#', 'Añadir plantilla', 'example-button', 'Añade una plantilla a la página');
+			TportleltLink.onclick = Tags.createFormWindow;
+		}
+	}
+
+	if (mw.config.get('wgNamespaceNumber') === 2 || mw.config.get('wgNamespaceNumber') === 3 || (mw.config.get('wgPageName').indexOf("Especial:Contribuciones") > -1)) {
+		let RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'example-button', 'Informa de un problema en relación con el usuario');
+		RportletLink.onclick = Reports.createFormWindow;
+	} else {
+		console.log("Non-user page: Reports will therefore not be loaded.")
 	}
 }
 
-if (mw.config.get('wgNamespaceNumber') === 2 || mw.config.get('wgNamespaceNumber') === 3 || (mw.config.get('wgPageName').indexOf("Especial:Contribuciones") > -1)) {
-	let RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'example-button', 'Informa de un problema en relación con el usuario');
-	RportletLink.onclick = Reports.createFormWindow;
-} else {
-	console.log("Non-user page: Reports will therefore not be loaded.")
+const loadTwinkleLite = async () => {
+	try {
+		await loadMoreBits();
+		await loadMoreBitsCSS();
+		initializeTwinkleLite();
+	} catch (error) {
+		console.log("Failed to load Twinkle Lite:", error)
+	}
 }
+
+loadTwinkleLite();
