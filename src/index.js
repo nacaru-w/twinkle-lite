3f8f9977886c9aba731f38b76b850cfdcc14779e
nacaru-w <ignaciocr92@gmail.com>
2023-06-17 02:07:58 +0200

    Modifying loading functions

--- src/index.js
+++ src/index.js
@@ -33,17 +33,16 @@ import * as Tags from "./modules/tags";
 // 	console.log("Non-user page: Reports will therefore not be loaded.")
 // }
 
-const loadMoreBits = () => {
-	return new Promise((resolve, reject) => {
-		mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript', 'text/javascript', resolve, reject);
-	});
+const loadDependencies = (callback) => {
+	mw.loader.using(['mediawiki.user', 'mediawiki.util', 'mediawiki.Title', 'jquery.ui', 'mediawiki.api', 'mediawiki.ForeignApi']);
+	callback();
 }
 
-const loadMoreBitsCSS = () => {
-	return new Promise((resolve, reject) => {
-		mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css', resolve, reject)
-	});
-}
+const loadMorebits = (callback) => {
+	mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.js&action=raw&ctype=text/javascript', 'text/javascript');
+	mw.loader.load('https://en.wikipedia.org/w/index.php?title=MediaWiki:Gadget-morebits.css&action=raw&ctype=text/css', 'text/css');
+	callback()
+};
 
 const initializeTwinkleLite = () => {
 	console.log("Loading Twinkle Lite...");
@@ -51,7 +50,7 @@ const initializeTwinkleLite = () => {
 	if (mw.config.get('wgNamespaceNumber') < 0 || !mw.config.get('wgArticleId')) {
 		console.log("Special or non-existent page: PP will therefore not be loaded.");
 		console.log("Special or non-existent page: DRM will therefore not be loaded.");
-		console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.")
+		console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.");
 	} else {
 		let DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
 		DRMportletLink.onclick = DeletionRequestMaker.createFormWindow;
@@ -69,18 +68,16 @@ const initializeTwinkleLite = () => {
 		let RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'example-button', 'Informa de un problema en relación con el usuario');
 		RportletLink.onclick = Reports.createFormWindow;
 	} else {
-		console.log("Non-user page: Reports will therefore not be loaded.")
+		console.log("Non-user page: Reports will therefore not be loaded.");
 	}
-}
+};
 
-const loadTwinkleLite = async () => {
-	try {
-		await loadMoreBits();
-		await loadMoreBitsCSS();
-		initializeTwinkleLite();
-	} catch (error) {
-		console.log("Failed to load Twinkle Lite:", error)
-	}
-}
+const loadTwinkleLite = () => {
+	loadDependencies(() => {
+		loadMorebits(() => {
+			initializeTwinkleLite();
+		});
+	})
+};
 
 loadTwinkleLite();
