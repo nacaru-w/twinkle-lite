4be7e70763b14a18b31284a8843a907c049d8a27
nacaru-w <ignaciocr92@gmail.com>
2023-07-19 13:06:10 +0200

    Adding warn module

---
+++ src/modules/warns.js
@@ -0,0 +1,5 @@
+// ** Warn module **
+// Posts a warning message on a user discussion page that can be selected as part of a series of options of a checkbox list
+
+import * as utils from "./utils";
+
