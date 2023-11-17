398ae7566c80d53f11e0150ea7dc081a85e0bb55
nacaru-w <ignaciocr92@gmail.com>
2023-11-17 11:36:26 +0100

    Modifying portlet button id

--- src/index.js
+++ src/index.js
@@ -25,29 +25,29 @@ const initializeTwinkleLite = () => {
 		console.log("Special or non-existent page: DRM will therefore not be loaded.");
 		console.log("Special or non-existent page: Speedy deletion will therefore not be loaded.");
 	} else {
-		const DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'example-button', 'Abre una consulta de borrado para esta página');
+		const DRMportletLink = mw.util.addPortletLink('p-cactions', '#', 'Abrir CDB', 'TL-button', 'Abre una consulta de borrado para esta página');
 		if (DRMportletLink) {
 			DRMportletLink.onclick = DeletionRequestMaker.createFormWindow;
 		}
-		const PPportletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'example-button', 'Solicita que esta página sea protegida');
+		const PPportletLink = mw.util.addPortletLink('p-cactions', '#', 'Pedir protección', 'TL-button', 'Solicita que esta página sea protegida');
 		if (PPportletLink) {
 			PPportletLink.onclick = PageProtection.createFormWindow;
 		}
-		const SDportletLink = mw.util.addPortletLink('p-cactions', '#', 'Borrado rápido', 'example-button', 'Solicita el borrado rápido de la página');
+		const SDportletLink = mw.util.addPortletLink('p-cactions', '#', 'Borrado rápido', 'TL-button', 'Solicita el borrado rápido de la página');
 		if (SDportletLink) {
 			SDportletLink.onclick = SpeedyDeletion.createFormWindow;
 		}
 	}
 
 	if (currentNamespace === 0 || currentNamespace === 104) {
-		const TportleltLink = mw.util.addPortletLink('p-cactions', '#', 'Añadir plantilla', 'example-button', 'Añade una plantilla a la página');
+		const TportleltLink = mw.util.addPortletLink('p-cactions', '#', 'Añadir plantilla', 'TL-button', 'Añade una plantilla a la página');
 		TportleltLink.onclick = Tags.createFormWindow;
 	}
 
 	if (currentNamespace === 2 || currentNamespace === 3 || (mw.config.get('wgPageName').indexOf("Especial:Contribuciones") > -1)) {
-		const RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'example-button', 'Informa de un problema en relación con el usuario');
+		const RportletLink = mw.util.addPortletLink('p-cactions', '#', 'Denunciar usuario', 'TL-button', 'Informa de un problema en relación con el usuario');
 		RportletLink.onclick = Reports.createFormWindow;
-		const WportletLink = mw.util.addPortletLink('p-cactions', '#', 'Avisar al usuario', 'example-button', 'Deja una plantilla de aviso al usuario en su página de discusión');
+		const WportletLink = mw.util.addPortletLink('p-cactions', '#', 'Avisar al usuario', 'TL-button', 'Deja una plantilla de aviso al usuario en su página de discusión');
 		WportletLink.onclick = Warns.createFormWindow;
 	} else {
 		console.log("Non-user page: Reports will therefore not be loaded.");
