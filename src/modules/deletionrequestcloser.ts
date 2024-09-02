4d64bc73a4bda86f08d3cec5bff82a014e15dca5
nacaru-w <ignaciocr92@gmail.com>
2024-09-02 13:58:12 +0200

    DRC: finished form object and structured future API calls

--- src/modules/deletionrequestcloser.ts
+++ src/modules/deletionrequestcloser.ts
@@ -1,7 +1,9 @@
-import { SimpleWindowInstance } from "types/morebits-types";
-import { currentPageName, today } from "utils/utils";
+import { ListElementData, SimpleWindowInstance } from "types/morebits-types";
+import { createStatusWindow, currentPageName, today } from "../utils/utils";
 
 let Window: SimpleWindowInstance;
+
+const closureOptions: string[] = ['Mantener', 'Borrar', 'Otro'];
 
 const DRC = {
     closedDR: {
@@ -21,7 +23,56 @@ const DRC = {
     }
 }
 
+function getClosureOptions(): ListElementData[] {
+    return closureOptions.map((e) => {
+        return { type: 'option', value: e, label: e }
+    })
+}
+
+function manageOtherInputField(selectedOption: string): void {
+    console.log(selectedOption)
+    const field = document.getElementById('otherField');
+    const fieldParent = field?.parentElement
+
+    if (fieldParent) {
+        switch (selectedOption) {
+            case 'Otro':
+                fieldParent.style.display = 'flex';
+                fieldParent.style.marginBlock = '5px';
+                field.style.display = 'block';
+                if (!document.getElementById('otherFieldLabel')) {
+                    const label = document.createElement('label')
+                    label.id = 'otherFieldLabel';
+                    label.innerText = 'Escribe el resultado de la consulta: ';
+                    fieldParent.prepend(label);
+                }
+                break;
+            default:
+                fieldParent.style.display = 'none';
+        }
+    }
+}
+
+async function editRequestPage(decision: string, comment?: string) {
+
+}
+
+async function editArticle(decision: string) {
+
+}
+
+async function editArticleTalkPage(decision: string) {
+
+}
+
 function submitMessage(e: Event) {
+    const form = e.target;
+    const input = Morebits.quickForm.getInputData(form);
+
+    createStatusWindow(new Morebits.simpleWindow(400, 350));
+    new Morebits.status("Paso 1", "editando la página de la consulta", "info");
+
+    console.log(input);
 
 }
 
@@ -34,8 +85,34 @@ export function createDRCFormWindow() {
 
     const form = new Morebits.quickForm(submitMessage);
 
+    form.append({
+        type: 'select',
+        name: 'result',
+        label: 'Selecciona el resultado de la consulta:',
+        list: getClosureOptions(),
+        event: (e: any) => manageOtherInputField(e.target.value)
+    })
+
+    form.append({
+        type: 'input',
+        name: 'otherField',
+        id: 'otherField',
+        style: 'margin-left: 3px; display: none;'
+    })
+
+    form.append({
+        type: 'textarea',
+        name: 'reason',
+        label: 'Añade un comentario (opcional)',
+        tooltip: 'Añade un comentario aclaratorio que complemente a la decisión tomada. Este aparecerá anexo a la decisión. Puedes usar wikicódigo y no es necesario firmarlo.'
+    })
+
+    form.append({
+        type: 'submit',
+        label: 'Aceptar'
+    });
+
     const result = form.render();
     Window.setContent(result);
     Window.display();
-
 }
