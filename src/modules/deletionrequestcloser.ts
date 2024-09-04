1f4f79b1b43c8991383092f69026dec5faa37803
nacaru-w <ignaciocr92@gmail.com>
2024-09-05 00:15:56 +0200

    DRC: added function to interact with mw API
    Added functions dealing with the editing of the
    deletion request page and the editing or deletion of
    the original page

--- src/modules/deletionrequestcloser.ts
+++ src/modules/deletionrequestcloser.ts
@@ -1,5 +1,5 @@
-import { ListElementData, SimpleWindowInstance } from "types/morebits-types";
-import { createStatusWindow, currentPageName, today } from "../utils/utils";
+import { ListElementData, QuickFormInputObject, SimpleWindowInstance } from "types/morebits-types";
+import { createStatusWindow, currentPageName, deletePage, getContent, today } from "../utils/utils";
 
 let Window: SimpleWindowInstance;
 
@@ -7,8 +7,8 @@ const closureOptions: string[] = ['Mantener', 'Borrar', 'Otro'];
 
 const DRC = {
     closedDR: {
-        top: (veredict: string, comment?: string): string => {
-            return `{{cierracdb-arr}} '''${veredict.toUpperCase}'''. ${comment} ~~~~`;
+        top: (veredict: string, comment: string | null): string => {
+            return `{{cierracdb-arr}} '''${veredict.toUpperCase}'''. ${comment ? comment + ' ' : ''}~~~~`;
         },
         bottom: '{{cierracdb-ab}}'
     },
@@ -53,26 +53,78 @@ function manageOtherInputField(selectedOption: string): void {
     }
 }
 
-async function editRequestPage(decision: string, comment?: string) {
-
+function replaceDRTemplate(input: string, replacement: string): string {
+    const templateRegex = /\{\{RETIRA ESTA PLANTILLA CUANDO CIERRES ESTA CONSULTA\|[^\}]+\}\}/;
+    return input.replace(templateRegex, replacement);
 }
 
-async function editArticle(decision: string) {
+function extractPageTitleFromWikicode(input: string): string | null {
+    // Regular expression to match the pattern with variable "=" and capture the content inside the square brackets
+    const match = input.match(/=+\s*\[\[(.+?)\]\]\s*=+/);
 
+    // If there's a match, return the captured group (the content inside the brackets)
+    return match ? match[1] : null;
+}
+
+async function editRequestPage(decision: string, comment: string | null) {
+    new Morebits.status("Paso 1", "cerrando la página de la consulta...", "info");
+    await new mw.Api().edit(
+        currentPageName,
+        (revision: any) => ({
+            text: replaceDRTemplate(revision.content, DRC.closedDR.top(decision, comment)) + '\n' + DRC.closedDR.bottom,
+            summary: `Cierro [[${currentPageName}]] con resultado ${decision.toUpperCase()} mediante [[WP:TL|Twinkle Lite]]`,
+            minor: false
+        })
+    )
+}
+
+async function editArticle(decision: string): Promise<void> {
+    const content = await getContent(currentPageName);
+    const page = extractPageTitleFromWikicode(content);
+    if (page) {
+        if (decision == 'Borrar') {
+            new Morebits.status("Paso 2", "borrando la página original...", "info");
+            const reason = `Según resultado de CDB: [[${currentPageName}]]`
+            await deletePage(page, true, reason)
+        } else {
+            new Morebits.status("Paso 2", "editando la página original...", "info");
+            await new mw.Api().edit(
+                page,
+                (revision: any) => ({
+                    text: DRC.articlePage.removeTemplate(revision.content),
+                    summary: `Elimino plantilla según el resultado de [[${currentPageName}]]: ${decision.toUpperCase()} mediante [[WP:TL|Twinkle Lite]]`,
+                    minor: false
+                })
+            );
+            new Morebits.status("Paso 3", "editando la página de discusión...", "info");
+            // TODO
+        }
+    }
 }
 
 async function editArticleTalkPage(decision: string) {
 
 }
 
-function submitMessage(e: Event) {
+async function submitMessage(e: Event) {
     const form = e.target;
-    const input = Morebits.quickForm.getInputData(form);
+    const input: QuickFormInputObject = Morebits.quickForm.getInputData(form);
+
+    const decision: string = input.result !== 'Otro' ? input.result : input.otherField;
+    const comment: string | null = input.reason ? input.reason : null;
 
     createStatusWindow(new Morebits.simpleWindow(400, 350));
     new Morebits.status("Paso 1", "editando la página de la consulta", "info");
 
     console.log(input);
+
+    try {
+        await editRequestPage(decision, comment);
+        await editArticle(decision);
+    } catch (error) {
+        new Morebits.status("❌ Se ha producido un error", "Comprueba las ediciones realizadas", "error");
+        console.log(`Error: ${error}`);
+    }
 
 }
 
