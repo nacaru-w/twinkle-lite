5d1c2b55ecc5e432a5500db1ea82947a3b257dff
nacaru-w <ignaciocr92@gmail.com>
2024-10-23 23:12:45 +0200

    Created fields for each module

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -1,4 +1,4 @@
-import { SimpleWindowInstance } from "types/morebits-types";
+import { QuickForm, QuickFormElementInstance, SimpleWindowInstance } from "types/morebits-types";
 
 let Window: SimpleWindowInstance;
 
@@ -14,6 +14,56 @@ export function createConfigWindow() {
     Window.addFooterLink('Documentación de Twinkle Lite', 'WP:TL')
 
     const form = new Morebits.quickForm(submitMessage);
+
+    const tagsField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de plantillado de artículos'
+    });
+
+    const DRCField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de cerrado de consultas de borrado'
+    });
+
+    const DRMField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de apertura de consultas de borrado'
+    });
+
+    const fastBlockerField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de bloqueado rápido'
+    });
+
+    const blockAppealsField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de resolución de peticiones de desbloqueo'
+    });
+
+    const hideField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de ocultado de ediciones'
+    });
+
+    const pageProtectionField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de solicitud de protección de páginas'
+    });
+
+    const reportsField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de denuncias'
+    });
+
+    const speedyDeletionField: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de solicitud de borrado rápido'
+    });
+
+    const warningsModule: QuickFormElementInstance = form.append({
+        type: 'field',
+        label: 'Módulo de advertencias'
+    });
 
     const result = form.render();
     Window.setContent(result);
