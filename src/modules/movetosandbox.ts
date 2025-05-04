d57a0be03df5153a49a8f1b1e6de30e8e7300f44
nacaru-w <ignaciocr92@gmail.com>
2025-05-04 11:34:03 +0200

    [UPD] MTS: added function that deals with leaving a message on creator's TP

--- src/modules/movetosandbox.ts
+++ src/modules/movetosandbox.ts
@@ -1,5 +1,5 @@
-import { QuickFormElementInstance, SimpleWindowInstance } from "types/morebits-types";
-import { createStatusWindow, currentPageName, finishMorebitsStatus, getContent, getCreator, isPageMissing, movePage } from "./../utils/utils";
+import { QuickFormElementInstance, QuickFormInputObject, SimpleWindowInstance } from "types/morebits-types";
+import { createPage, createStatusWindow, currentPageName, finishMorebitsStatus, getContent, getCreator, isPageMissing, movePage } from "./../utils/utils";
 
 let Window: SimpleWindowInstance;
 let creator: string | null;
@@ -33,8 +33,14 @@ export async function createMTSFormWindow() {
     Window.display();
 }
 
+/**
+ * Calls the MW API to move the current article to the user's sandbox page.
+ * If it's in use, it moves it into a subpage
+ */
 async function movePageToSandbox() {
-    new Morebits.status(`Paso ${step + 1}`, "moviendo la página al taller del usuario", "info");
+    step += 1;
+
+    new Morebits.status(`Paso ${step}`, "moviendo la página al taller del usuario", "info");
 
     const sandbox = `Usuario:${creator}/Taller`;
     const isSandboxEmpty = await isPageMissing(sandbox);
@@ -48,17 +54,48 @@ async function movePageToSandbox() {
     });
 }
 
-async function postMessageOnTalkPage() {
-    console.log('postmessageontalkpage called')
+/**
+ * Leaves a warning template on the creator's talk page, creating one if it doesn't exist
+ * @param moveReason - the reason why the page was moved as described in the form
+ */
+async function postMessageOnTalkPage(moveReason: string) {
+    step += 1;
+
+    new Morebits.status(`Paso ${step}`, "publicando un mensaje en la página de discusión del creador...", "info");
+    const summaryMessage = `Avisando al usuario del traslado de su artículo ${currentPageName} al [[${destinationPage}|taller]] mediante [[WP:TL]]`;
+    const talkPageTemplate = `{{Aviso traslado al taller|${currentPageName}|${destinationPage.endsWith('/Taller') ? '' : currentPageName}|razón=${moveReason}}}`;
+    const isTalkEmpty = await isPageMissing(`Usuario_discusión:${creator}`)
+    if (isTalkEmpty) {
+        return await createPage(
+            `Usuario_discusión:${creator}`,
+            talkPageTemplate,
+            summaryMessage
+        )
+    } else {
+        return await new mw.Api().edit(
+            `Usuario_discusión:${creator}`,
+            (revision: any) => {
+                return {
+                    text: revision.content + `\n${talkPageTemplate}`,
+                    summary: summaryMessage,
+                    minor: false
+                }
+            }
+        )
+    }
 }
 
 async function submitMessage(e: Event) {
+    const form = e.target as HTMLFormElement;
+    const input: QuickFormInputObject = Morebits.quickForm.getInputData(form);
+    const moveReason = input.reason;
+
     const statusWindow: SimpleWindowInstance = new Morebits.simpleWindow(400, 350);
     createStatusWindow(statusWindow);
 
     try {
-        await movePageToSandbox()
-        postMessageOnTalkPage();
+        await movePageToSandbox();
+        await postMessageOnTalkPage(moveReason);
         finishMorebitsStatus(Window, statusWindow, 'finished', true);
     } catch (error) {
         finishMorebitsStatus(Window, statusWindow, 'error');
