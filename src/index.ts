27ef110a89556b06e9998d53c566ff28c49e7d3a
nacaru-w <ignaciocr92@gmail.com>
2025-05-18 23:09:20 +0200

    [UPD] Controlling config preferences for MTS module

--- src/index.ts
+++ src/index.ts
@@ -143,13 +143,18 @@ if (!window.IS_TWINKLE_LITE_LOADED) {
 			if (configLink) {
 				configLink.onclick = () => createConfigWindow(settings);
 			}
+
+
 		}
 
 		// Move to sandbox module option
 		if (currentNamespace == 0 || currentNamespace == 1 || currentNamespace == 104 || currentNamespace == 105) {
-			const mtsLink = mw.util.addPortletLink('p-cactions', 'javascript:void(0)', 'Mover al taller del usuario', 'TL-button', 'Mover esta página la taller del usuario');
-			if (mtsLink) {
-				mtsLink.onclick = () => createMTSFormWindow();
+			if (settings?.MTSActionsMenuCheckbox ?? true) {
+
+				const mtsLink = mw.util.addPortletLink('p-cactions', 'javascript:void(0)', 'Mover al taller del usuario', 'TL-button', 'Mover esta página la taller del usuario');
+				if (mtsLink) {
+					mtsLink.onclick = () => createMTSFormWindow();
+				}
 			}
 		}
 
