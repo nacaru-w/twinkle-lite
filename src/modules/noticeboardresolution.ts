b9639f2e8e03d2a0f7bb3c04232303dcebfd0c41
nacaru-w <ignaciocr92@gmail.com>
2025-10-18 23:38:55 +0200

    [UPD] NBR: added API section content fetch and edition

--- src/modules/noticeboardresolution.ts
+++ src/modules/noticeboardresolution.ts
@@ -2,24 +2,72 @@
 // Handles the resolution of noticeboard entries, including fetching and updating entries.
 
 import { QuickFormInputObject, SimpleWindowInstance } from "types/morebits-types";
-import { currentPageName, extractNoticeboardTitle } from "./../utils/utils";
+import { currentPageName, extractNoticeboardTitle, api, getContent, createStatusWindow, finishMorebitsStatus, editPage } from "./../utils/utils";
 
 
 
 let Window: SimpleWindowInstance;
-let requestId: string;
+let step = 0;
+let requestInfo: { title: string; sectionNumber: number | string } | null;
 
 async function submitMessage(event: Event): Promise<void> {
     const form = event.target as HTMLFormElement;
     const input: QuickFormInputObject = Morebits.quickForm.getInputData(form);
     console.log(input);
+
+    const statusWindow: SimpleWindowInstance = new Morebits.simpleWindow(400, 350)
+    createStatusWindow(statusWindow);
+
+    try {
+        await editSection(input.text);
+        location.reload();
+    } catch (error) {
+        finishMorebitsStatus(Window, statusWindow, 'error');
+        console.error(`Error: ${error}`);
+
+    }
+
 }
 
-export function createNoticeboardResolutionWindow(headingId: string): void {
+function replaceAnswerPlaceholder(sectionText: string, newText: string): string {
+    return sectionText.replace(
+        // match the line starting with "; Respuesta" and the text after it
+        /(; *Respuesta\s*\n)([\s\S]*?)(?=(?:\n;|$))/,
+        `$1${newText}\n`
+    );
+}
+
+async function appendResolutionText(newText: string, sectionNumber: string) {
+    new Morebits.status(`Paso ${step += 1}`, "añadiendo la resolución a la sección...", "info");
+
+    return await editPage(
+        currentPageName,
+        "Resolviendo resolución mediante [[WP:TL|Twinkle Lite]]",
+        newText,
+        sectionNumber
+    );
+
+}
+
+
+async function editSection(sysopResolution: string): Promise<void> {
+    new Morebits.status(`Paso ${step += 1}`, "obteniendo el contenido de la sección...", "info");
+    const sectionContent = await getContent(currentPageName, requestInfo?.sectionNumber?.toString());
+    if (sectionContent) {
+        const newSectionContent = replaceAnswerPlaceholder(sectionContent, sysopResolution);
+        if (newSectionContent) {
+            await appendResolutionText(newSectionContent, requestInfo?.sectionNumber?.toString() || '');
+        }
+        return
+    }
+}
+
+export function createNoticeboardResolutionWindow(headerInfo: { title: string; sectionNumber: number | string } | null): void {
     // Includes request title with underscores as spaces and, if there are more than one
     // requests with the same title, the number represeting its position in descending
     // chronological order (1 being the oldest).
-    requestId = headingId;
+    requestInfo = headerInfo;
+    console.log(requestInfo)
 
     Window = new Morebits.simpleWindow(620, 530);
     Window.setScriptName('Twinkle Lite');
