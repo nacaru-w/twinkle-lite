305630c94003a95abdbd24b0529b016e11dc6691
nacaru-w <ignaciocr92@gmail.com>
2025-12-24 19:04:29 +0100

    [UPD] Restructured utils functions

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,15 @@
+export const spanishMonths: Record<string, number> = {
+    enero: 1,
+    febrero: 2,
+    marzo: 3,
+    abril: 4,
+    mayo: 5,
+    junio: 6,
+    julio: 7,
+    agosto: 8,
+    septiembre: 9,
+    setiembre: 9,
+    octubre: 10,
+    noviembre: 11,
+    diciembre: 12,
+};
